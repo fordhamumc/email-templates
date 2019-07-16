ef55b2d766d931b2d19d07f86b197d0f9e6415bb
Michael Foley <michaeldfoley@gmail.com>
2019-07-16 14:16:51 -0400

    created template for president's office

---
+++ pages/president.tsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import EmailViewer from "../layouts/EmailViewer";
+import Header from "../components/Header";
+import { Container, GlobalScaffold } from "../components/Container";
+import Footer from "../components/Footer";
+import { sizes } from "../components/defaults";
+
+export default () => (
+  <EmailViewer>
+    <Fragment>
+      <GlobalScaffold />
+      <Header
+        src="https://www.fordham.edu/images/fordham_logo.png"
+        alt="Fordham University"
+        width="200"
+      >
+        Office of the President
+      </Header>
+      <Container role="main" maxWidth={sizes.innerWidth + 30} bottomPad={true}>
+        Put your content here...
+      </Container>
+      <Footer role="contentinfo">&nbsp;</Footer>
+    </Fragment>
+  </EmailViewer>
+);
